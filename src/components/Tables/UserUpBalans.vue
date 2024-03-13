8774cdca2894da55aeffc583b0e88a3c8194b79a
urzp <ermak80_pass@mail.ru>
2024-03-13 15:21:01 +0300

    complit user pages

--- src/components/Tables/UserUpBalans.vue
+++ src/components/Tables/UserUpBalans.vue
@@ -1,11 +1,11 @@
 <template>
   <BackGrCard>
     <div class="table">
-      <TitleTable title="Пополнение баланса пользователя" :subtitle="`от ${date_f.from} до ${date_f.until}`" @click="date_f_click = !date_f_click" class="pointer"/>
+      <TitleTable title="Баланс пользователя" :subtitle="`от ${date_f.from} до ${date_f.until}`" />
       <TableHeader class="set_width_table">
         <h_colum title="#"/>
         <h_colum title='Дата время' type_f="date" v-model="date_f" :click_hand="date_f_click"/>
-        <h_colum title='Транзакция'  type_f='find' v-model="transaction_f"/>
+        <h_colum title='Транзакция'/>
         <h_colum title='Баланс до'/>
         <h_colum title='Сумма'/>
         <h_colum title='Баланс после' />
@@ -13,14 +13,14 @@
         <h_colum title=''/>
       </TableHeader>
       <TableBody>
-        <div v-for="(item, index) in part_payments" :key="item.id" class="row set_width_table" @click="pop_show(index)" >
+        <div v-for="(item, index) in part_payments" :key="item.id" class="row set_width_table" >
           <div>{{ index + 1 + (part - 1)*this.part_items}}</div>
           <div class="content_left">{{ item.datetime }}</div>
           <div class="content_right">{{ item.trnsaction }}</div> 
           <div class="content_right">{{ item.oldBalans}} p.</div>
           <div class="content_right">{{ item.sum }} p.</div>
           <div class="content_right">{{ item.newBalans }} p.</div>
-          <div class="content_left"></div>
+          <div class="content_left">{{ item.check }}</div>
           <div class="content_left"></div>
         </div>
       </TableBody>
@@ -44,18 +44,17 @@ EventBus.on('pageTable:update', (page)=>{
 },
 data(){
 return{
+  orders:'',
   payments:'',
   part:'',
   part_items: 100,
   f_payments:'',
-  part_payments:'',
+  part_payments:{},
   date_f:{
     until: this.getFromTodayString(),
     from:  '2024-01-01'
   },
   date_f_click: false,
-  transaction_f:'',
-  status_f:'',
   pop_order:{},
   pop_open:false,
 }
@@ -82,24 +81,48 @@ async updateList(){
   let result = await getData('getData.php',{typeData:'userPaumentBalans', user_id:this.user_id, date_from:this.date_f.from, date_until:this.date_f.until})
   if(!this.checkResult(result)) return false
   this.payments = await result.data
+  result = await getData('getData.php',{typeData:'userOrders', user_id:this.user_id, date_from:this.date_f.from, date_until:this.date_f.until})
+  if(!this.checkResult(result)) return false
+  this.orders = await result.data
   this.filter();
 },
 async filter(){
+  this.orders.forEach( (e, i) => {
+    this.orders[i].sum = "-"+ e.sum
+  });
+  let orders_success = this.orders.filter(i=>i.provader_status.indexOf('false')==-1)
+
   let result = []
   result.push(...this.payments)
-  let status_f = this.status_f
-  let transaction_f = this.transaction_f
-  if(transaction_f!=''){
-    result = result.filter(i=>i.trnsaction.indexOf(transaction_f)!=-1)
-  }
+  result.push(...orders_success)
+  result = result.sort((a,b)=>{
+    let res;
+    a = new Date(a.datetime)
+    b = new Date(b.datetime)
+    if(a > b) res = 1;
+    if(a == b) res = 0;
+    if(a < b) res = -1;
+    return res
+  }) 
 
-
-  if(status_f!=''){
-    if(status_f=='true') result = result.filter(i=>i.provader_status=='success')
-    if(status_f=='false') result = result.filter(i=>i.provader_status=='fall'||i.provader_status=='false')
-  }
+ 
   this.f_payments = result
+  this.checkBalanse()
   this.intPage()
+},
+checkBalanse(){
+  this.f_payments.forEach((e, i)=>{
+      if(i + 2 > this.f_payments.length) return false
+      let a = this.f_payments[i].newBalans
+      let b = this.f_payments[i+1].oldBalans
+      let result
+      if(a==b){
+        result = 'success'
+      }else{
+        result = 'fall'
+      }
+      this.f_payments[i+1]['check'] = result
+  })
 },
 checkResult(result){
   this.lading = false
