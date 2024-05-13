21fbdc30425308c1df3495d21755c15ac7d8b589
urzp <ermak80_pass@mail.ru>
2024-05-13 23:03:44 +0300

    save

--- src/components/complex/PopupSettingCardProduct.vue
+++ src/components/complex/PopupSettingCardProduct.vue
@@ -13,7 +13,42 @@
                     <div class="button"><div class="icon_btn"></div>НАСТРОИТЬ</div>
                 </div>
                 <div class="settings">
-
+                    <div class="title_settings">Настройка сервиса</div>
+                    <div class="servis_content_wrap">
+                        <div class="form_content">
+                            <div class="provider_form">
+                                <div class="title_field_form">Провайдер:</div>
+                                <div class="provider_field">
+                                    <div class="icon_btn_list_provider"><img src="@/assets/icons/btn_list.svg" alt=""></div>
+                                    <div class="content_provider">2 https://smmmain.com/api/v2</div>
+                                </div>
+                                <div class="list_providers_wrap">
+                                    <div class="list_providers"></div>
+                                </div>
+                            </div>
+                            <div class="settings_form">
+                                <div class="title_field_form">Параметры:</div>
+                                <div class="settings_field">
+                                    <div class="settings_field_header">
+                                        <img class="icon_setting_add" src="@/assets/icons/btn_add.svg" alt="">
+                                        <div class="header_colum header_colum_1">Название</div>
+                                        <div class="header_colum header_colum_2">Значение</div>
+                                    </div>
+                                    <div class="setting_field_row">
+                                        <div class="colum header_colum_1">id_servis</div>
+                                        <div class="colum header_colum_2">4</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form_test_buttons">
+                            <div class="form_test"></div>
+                            <div class="form_buttons">
+                                <div class="test_btn">ТЕСТ</div>
+                                <div class="save_btn">СОХРАНИТЬ</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -92,59 +127,196 @@ export default{
 }
 
 .icon{
-        height: 90px;
-        width: 90px;
-        margin: 0 auto;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: contain;
+    height: 90px;
+    width: 90px;
+    margin: 0 auto;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+}
+
+.title{
+    margin-top: 40px;
+    margin-bottom: 25px;
+    height: 52px;
+    font-size: 20px;
+    line-height: 30px;
+}
+
+.discription{
+    margin-top: 18px;
+    height: 120px;
+    font-family: 'TildaSans';
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 1.3;
+}
+
+.price{
+    margin-top: 25px;
+    font-size: 20px;
+}
+
+.button{
+    height: 45px;
+    width: 150px;
+    margin: auto;
+    margin-top: 34px;
+    display: flex;
+    color: #fff;
+    font-size: 14px;
+    background-color: #4555f0;
+    border-radius: 50px;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    .icon_btn{
+    background-image: url('../../assets/icons/menu_left/settings_w.svg');
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    }
+}
+
+.settings{
+    margin-left: 45px;
+    width: 60%;
+    
+    .title_settings{
+        text-align: center;
+        font-family: 'TildaSansBold';
+        font-size: 24px;
+        margin-top: 0px;
     }
 
-    .title{
-      margin-top: 40px;
-      margin-bottom: 25px;
-      height: 52px;
-      font-size: 20px;
-      line-height: 30px;
+    .servis_content_wrap{
+        display: flex;
     }
 
-    .discription{
-      margin-top: 18px;
-      height: 120px;
-      font-family: 'TildaSans';
-      font-weight: 300;
-      font-size: 14px;
-      line-height: 1.3;
+    .form_content{
+        width: 400px;
+        
+        .title_field_form{
+            font-family: 'TildaSans';
+            font-size: 14px;
+            text-align: left;
+            color: #969696;
+            margin-bottom: 5px;
+        }
+
+        .provider_form{
+            margin-bottom: 20px;
+        }
+
+        .provider_field{
+            height: 36px;
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            background-color: #F4F4F4;
+            border-radius: 30px;
+        }
+
+        .icon_btn_list_provider{
+            margin-left: 10px;
+            width: 30px;
+            border-right: 1px solid #D2D2D2;
+            cursor: pointer;
+        }
+
+        .content_provider{
+            color: #383838;
+        }
+
+        .list_providers_wrap{
+            position: relative;
+            .list_providers{
+                position: absolute;
+                height: 300px;
+                width: 390px;
+                background-color: #eaeaea;
+            }
+        }
+
+        .settings_field{
+            background-color: #F4F4F4;
+            border-radius: 20px;
+            height: 350px;
+            padding: 10px 20px;
+            .settings_field_header{
+                display: flex;
+                align-items: center;
+                gap: 10px;
+                border-bottom: 1px solid #D2D2D2;
+                color: #969696;
+                height: 35px;
+                .icon_setting_add{
+                    cursor: pointer;
+                }
+                .header_colum_1{
+                    width: 50%;
+                    border-right: 1px solid #D2D2D2;
+                    color: #969696;
+                }
+                .header_colum_2{
+                    width: 50%;
+                }
+            }
+
+            .setting_field_row{
+                display: flex;
+                align-items: center;
+                gap: 10px;     
+                height: 35px;
+                .header_colum_1{
+                    width: 60%;
+                }
+                .header_colum_2{
+                    width: 50%;
+                }
+            }
+        }
+
     }
 
-    .price{
-      margin-top: 25px;
-      font-size: 20px;
-    }
+    .form_test_buttons{
+        margin-top: 30px;
+        margin-left: 30px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .form_test{
+            width: 300px;
+            height: 370px;
+            background-color: #E2EFDA;
+            border-radius: 20px;
+        }
 
-    .button{
-      height: 45px;
-      width: 150px;
-      margin: auto;
-      margin-top: 34px;
-      display: flex;
-      color: #fff;
-      font-size: 14px;
-      background-color: #4555f0;
-      border-radius: 50px;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-
-      .icon_btn{
-        background-image: url('../../assets/icons/menu_left/settings_w.svg');
-        width: 20px;
-        height: 20px;
-        margin-right: 5px;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: contain;
-      }
+        .form_buttons{
+            display: flex;
+            justify-content: space-around;
+            div{
+                border-radius: 30px;
+                height: 45px;
+                width: 135px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+            .test_btn{
+                border: 1px solid #4A55F0;
+                color:#4A55F0;
+            }
+            .save_btn{
+                background-color:  #4A55F0;
+                color:#ffffff;
+            }
+        }
     }
+}
 
 </style>
