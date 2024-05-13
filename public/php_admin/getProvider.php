928c4c700224d8f32d865f1227a82e3a2f1d6562
urzp <ermak80_pass@mail.ru>
2024-05-13 19:03:23 +0300

    debag provider.php

--- public/php_admin/getProvider.php
+++ public/php_admin/getProvider.php
@@ -2,13 +2,15 @@
 
 include 'getAdminUserLevel.php';
 
+$id_prov = $rq_data -> provider_id;
+
 if($user_level=='admin'){
-    $sql = "SELECT * FROM `postavshik`";
+    $sql = "SELECT * FROM `postavshik` WHERE `id`='$id_prov'";
 }else{
-    $sql = "SELECT `id`, `id_old`, `balans`, `currency` FROM `postavshik`";
+    $sql = "SELECT `id`, `id_old`, `balans`, `currency` FROM `postavshik` WHERE `id`='$id_prov'";
 }
 
-$id_prov = $rq_data -> provider_id;
+
 
 $result_sql = $mysql -> query($sql);
 while ($row = $result_sql->fetch_assoc()) { 
