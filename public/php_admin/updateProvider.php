959971073190b24e73626528a58855c1cc19e1eb
urzp <ermak80_pass@mail.ru>
2024-07-09 10:59:29 +0300

    fix bag provider

--- public/php_admin/updateProvider.php
+++ public/php_admin/updateProvider.php
@@ -7,7 +7,7 @@ $id_old = $data -> id_old;
 $name = $data -> name;
 $api_key = $data -> api_key;
 
-if($id='new'){
+if($id=='new'){
     $sql = "INSERT INTO `postavshik` (`id_old`, `name`, `api_key`) VALUES ('$id_old', '$name', '$api_key')"; 
 }else{
     $sql = "UPDATE `postavshik` SET `id_old`='$id_old', `name`='$name', `api_key`='$api_key' WHERE `id`='$id' ";
